07516e08f9e7e2105364082fcf8f0ebb40a08ca8
Ayushkumar302 <ayushxkumar2002@gmail.com>
2026-08-01 20:34:26 +0530

    Make BASE_URL dynamic for mobile devices

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'http://localhost:5000/api/v1'
+const hostname = typeof window !== "undefined" && window.location.hostname ? window.location.hostname : "localhost";
+export const BASE_URL = `http://${hostname}:5000/api/v1`;
 export const token = localStorage.getItem("token");
